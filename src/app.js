ba9d181bf69278797acd2fb1110d772c485927e2
EBastidas <114364709+EmilyBastidas@users.noreply.github.com>
2025-10-22 19:49:08 +0000

    generador de excusas Emi

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let who = ['El perro', 'Un ladrón', 'un policía', 'mi abuela'];
+let action = ['quemó', 'perdió', 'se comió', 'rompió'];
+let what = ['mi tarea', 'mi notebook', 'mi celular'];
+let when = ['mientras me bañaba', 'cuando estaba durmiendo', 'mientras yo almorzaba', 'mientras yo estaba en el cine', 'mientras yo compraba desayuno'];
+
+let whorandom= Math.floor (Math.random () * 4);
+let actionrandom= Math.floor (Math.random () * 4);
+let whatrandom= Math.floor (Math.random () * 3);
+let whenrandom= Math.floor (Math.random () * 5);
+
+let excuse = `${who[whorandom]} ${action[actionrandom]} ${what[whatrandom]} ${when[whenrandom]}`
+
+document.getElementById("excuse").innerHTML= excuse;}
